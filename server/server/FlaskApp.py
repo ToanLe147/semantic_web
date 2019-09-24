048552d01219b31742e05e98e42b5e159875c067
toanle147 <leductoan1210@gmail.com>
2019-09-24 07:28:19 +0300

    Connected Flask and ROS

--- server/server/FlaskApp.py
+++ server/server/FlaskApp.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template
+import roslibpy
+
 
 app = Flask(__name__)
+client = roslibpy.Ros(host='localhost', port=9090)
+client.run()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#
-# if __name__ == '__main__':
-#     app.run()
+
+if __name__ == '__main__':
+    app.run(host="192.168.100.16")
