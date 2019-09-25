185392247f8091438ef89ccee1666b8aeec9bd71
toanle147 <leductoan1210@gmail.com>
2019-09-25 23:03:40 +0300

    Update knowledge base and ontology handlers

--- server/server/FlaskApp.py
+++ server/server/FlaskApp.py
@@ -4,22 +4,26 @@ import sys
 modpath = '/home/nico/catkin_ws/src/semantic_web/src'
 sys.path.insert(0, modpath)
 from flask_socketio import SocketIO
+import threading
 
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+test = 10001
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', test=test)
+    return render_template('index.html')
 
 
-@socketio.on('connect')
-def handle_my_custom_event():
+@socketio.on('update')
+def handle_my_custom_event(msg):
     global test
+    test += 1
     socketio.emit('my response', test, callback="Message Received")
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="192.168.100.16")
+    # socketio.run(app, host="192.168.100.16")
+    socketio.run(app, host="localhost")
--- src/uploader.py
+++ src/uploader.py
@@ -1,109 +1,77 @@
 #!/usr/bin/env python
 import requests
 import json
+import ast  # convert string represent python expression to python expression
 
 
 class ontology():
     def __init__(self):
-        self.detected_objects = []
-        self.ontology_server = 'http://192.168.100.16:3030/Testing/'
+        # self.ontology_server = 'http://192.168.100.16:3030/Brainstorm/'
+        self.ontology_server = 'http://localhost:3030/Brainstorm/'
         self.header = {'content-type': 'application/x-www-form-urlencoded'}
-        self.prefix = ('PREFIX brainstorm:<http://www.semanticweb.org/led/ontologies/2019/4/brainstorm.owl#> '
+        self.prefix = ('prefix kbase: <http://www.semanticweb.org/nico/ontologies/2019/8/Kbase#> '
                        + 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> '
                        + 'PREFIX owl: <http://www.w3.org/2002/07/owl#> '
                        + 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> '
                        + 'PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> ')
 
-    def update_ontology(self, object, type):
-        # type is DELETE or INSERT
-        name, raw_string, corners = object.split("/")
-        location = raw_string.split(" ")
-        box = corners.split(" ")
-        if name not in self.detected_objects:
-            self.detected_objects.append(name)
-            # Prepair message
-            insert = (' Data { '
-                      + 'brainstorm:{}_status a owl:NamedIndividual , '.format(name)
-                      + 'brainstorm:informations ; '
-                      + 'brainstorm:xmin {}; '.format(box[1])
-                      + 'brainstorm:xmax {}; '.format(box[3])
-                      + 'brainstorm:ymin {}; '.format(box[0])
-                      + 'brainstorm:ymax {}. '.format(box[2])
+    def update_property(self, name, property, *argv):
+        '''
+        This method use to send request to update property value of an
+        instance. If there is data parameter, the method gives updating. If
+        there is no data parameter, the method gives default value of property.
+          param name:     instance's name - string
+          param property: property of mentioned instance - string
+          param argv:     data of property - string
+        '''
+        if argv:
+            value = argv[0]
+        else:
+            value = ""
 
-                      + 'brainstorm:{}_location a owl:NamedIndividual , '.format(name)
-                      + 'brainstorm:informations ; '
-                      + 'brainstorm:x {}; '.format(location[0])
-                      + 'brainstorm:y {}; '.format(location[1])
-                      + 'brainstorm:z {}. '.format(location[2])
+        # Prepair message.
+        update = ('DELETE'
+                  + ' WHERE { '
+                  + 'kbase:{0} kbase:{1} ?DATA'.format(name, property)
+                  + '};'
+                  + 'INSERT'
+                  + 'DATA { '
+                  + 'kbase:{0} kbase:{1} {2}'.format(name, property, value)})
 
-                      + 'brainstorm:{} a owl:NamedIndividual , brainstorm:objects ;'.format(name)
-                      + ' brainstorm:hasLocation brainstorm:{}_location; '.format(name)
-                      + 'brainstorm:hasStatus brainstorm:{}_status.'.format(name)
-                      + '}'
-                      )
-            # send POST request to server
-            msg = {'update': self.prefix + type + insert}
-            requests.post(url=self.ontology_server+'update', headers=self.header, data=msg)
-        return True
-
-    def get_info(self, name):
-        query = ("SELECT ?x ?y ?z ?xmin ?ymin ?xmax ?ymax " +
-                 "WHERE { " +
-                 "brainstorm:{}_location brainstorm:x ?x. ".format(name) +
-                 "brainstorm:{}_location brainstorm:y ?y. ".format(name) +
-                 "brainstorm:{}_location brainstorm:z ?z. ".format(name) +
-                 "brainstorm:{}_status brainstorm:xmin ?xmin. ".format(name) +
-                 "brainstorm:{}_status brainstorm:ymin ?ymin. ".format(name) +
-                 "brainstorm:{}_status brainstorm:xmax ?xmax. ".format(name) +
-                 "brainstorm:{}_status brainstorm:ymax ?ymax. ".format(name) +
-                 "}")
         # send POST request to server
-        msg = {'query': self.prefix + query}
-        r = requests.post(url=self.ontology_server+'query', headers=self.header, data=msg)
-        # Handle response as json type
-        res = json.loads(r.content)
-        location = [
-            float(res["results"]["bindings"][0]["x"]["value"]),
-            float(res["results"]["bindings"][0]["y"]["value"]),
-            float(res["results"]["bindings"][0]["z"]["value"])]
-        status = [
-            float(res["results"]["bindings"][0]["xmin"]["value"]),
-            float(res["results"]["bindings"][0]["ymin"]["value"]),
-            float(res["results"]["bindings"][0]["xmax"]["value"]),
-            float(res["results"]["bindings"][0]["ymax"]["value"])]
-        return {name: {"location": location, "status": status}}
+        msg = {'update': self.prefix + update}
+        requests.post(url=self.ontology_server+'update', headers=self.header, data=msg)
 
-    def get_name(self):
-        objects = []
-        query = ("SELECT ?name " +
-                 "WHERE {?name a  owl:NamedIndividual , brainstorm:objects .}")
-        msg = {'query': self.prefix + query}
-        r = requests.post(url=self.ontology_server+'query', headers=self.header, data=msg)
-        res = json.loads(r.content)
-        for i in res["results"]["bindings"]:
-            _, name = i["name"]["value"].split("#")
-            objects.append(name)
-        return objects
 
-    def get_relative_position(self, name):
-        left = []
-        right = []
-        up = []
-        down = []
-        ref_object = self.get_info(name)[name]['status']
-        for object in self.detected_objects:
-            if name == object:
-                continue
+    def handle_instance(self, name, type, action):
+        '''
+        This method use to send request to add or delete an instance.
+          param name:    instance's name - string
+          param type:    instance's class - string
+          param action:  action - INSERT or DELETE
+        '''
 
-            target_object = self.get_info(object)[object]['status']
+        # Prepair message.
+        update = ('{}'.format(action)
+                    + ' DATA { '
+                    + 'kbase:{0} a owl:NamedIndividual, kbase:{1};'.format(name, type)
+                    + 'kbase:Current_state  "" ;'
+                    + 'kbase:Data           "" ;'
+                    + 'kbase:Initial_state  "" ;'
+                    + 'kbase:Position       "" ;'
+                    + 'kbase:Status         "" .}')
 
-            if target_object[0] > ref_object[2]:
-                right.append(object)
-            if target_object[2] < ref_object[0]:
-                left.append(object)
-            if target_object[1] > ref_object[3]:
-                up.append(object)
-            if target_object[3] < ref_object[1]:
-                down.append(object)
+        delete = ('{}'.format(action)
+                    + ' WHERE { '
+                    + 'kbase:{0} ?PROP ?DATA'.format(name)
+                    + '}')
 
-        return {"LEFT": left, "RIGHT": right, "UP": up, "DOWN": down}
+        # send POST request to server
+        if action = "delete":
+            content = delete
+        if action = "insert":
+            content = update
+        msg = {'update': self.prefix + content}
+        requests.post(url=self.ontology_server+'update', headers=self.header, data=msg)
+
+    def query
