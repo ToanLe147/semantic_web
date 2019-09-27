4b45711f9a6a7b8be5f062227998f056e51fddf6
toanle147 <leductoan1210@gmail.com>
2019-09-28 00:24:09 +0300

    Finish Knowledge Base part in Web UI

--- server/server/FlaskApp.py
+++ server/server/FlaskApp.py
@@ -16,6 +16,14 @@ def index():
     return render_template('index.html')
 
 
+@socketio.on('refresh_data')
+def load_data():
+    data = KnowledgeBase.get_instances()
+    if data:
+        for i in data:
+            socketio.emit("refresh_page", i, callback="Update Page")
+
+
 @socketio.on('handle_instance')
 def update_instance(msg):
     name = msg["name"]
@@ -23,7 +31,10 @@ def update_instance(msg):
     action = msg["action"]
     print ("{} instance name {} {}".format(action, name, type))
     KnowledgeBase.handle_instance(name, type, action)
-    socketio.emit('server_response', "Updated", callback="Message Received")
+    new_msg = "Instance updated"
+    if action == "delete":
+        new_msg = "Instance deleted"
+    socketio.emit('server_response', new_msg, callback="Message Received")
 
 
 @socketio.on('handle_data')
@@ -32,10 +43,34 @@ def update_instance_data(msg):
     property = msg["type"]
     value = msg["value"]
     print ("{} instance type {} {}".format(instance, property, value))
-    KnowledgeBase.update_property(instance, property, value)
-    socketio.emit('server_response', "Updated", callback="Message Received")
+    KnowledgeBase.update_instance(instance, property, value)
+    socketio.emit('server_response', "Property updated", callback="Message Received")
+
+
+@socketio.on('refresh_relationship')
+def update_relationship():
+    data = KnowledgeBase.get_relationship()
+    if data:
+        for i in data:
+            socketio.emit("refresh_rel_list", i, callback="Update Page")
+
+
+@socketio.on('handle_relationship')
+def handle_relationship(msg):
+    print(len(msg))
+    data = []
+    if len(msg) > 1:
+        data = msg["relates"]
+        name1 = msg["name1"]
+        name2 = msg["name2"]
+        KnowledgeBase.handle_relationship(data, name1, name2)
+    else:
+        data = msg["relates"]
+        KnowledgeBase.handle_relationship(data)
+    if data:
+        socketio.emit('server_response', "Updated relationship", callback="Message Received")
 
 
 if __name__ == '__main__':
-    # socketio.run(app, host="192.168.100.16")
-    socketio.run(app, host="localhost")
+    socketio.run(app, host="192.168.100.16")
+    # socketio.run(app, host="localhost")
--- src/uploader.py
+++ src/uploader.py
@@ -9,13 +9,13 @@ class ontology():
         # self.ontology_server = 'http://192.168.100.16:3030/Brainstorm/'
         self.ontology_server = 'http://localhost:3030/Brainstorm/'
         self.header = {'content-type': 'application/x-www-form-urlencoded'}
-        self.prefix = ('prefix kbase: <http://www.semanticweb.org/nico/ontologies/2019/8/Kbase#> '
+        self.prefix = ('PREFIX kbase: <http://www.semanticweb.org/nico/ontologies/2019/8/Kbase#> '
                        + 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> '
                        + 'PREFIX owl: <http://www.w3.org/2002/07/owl#> '
                        + 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> '
                        + 'PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> ')
 
-    def update_property(self, name, property, *argv):
+    def update_instance(self, name, property, *argv):
         '''
         This method use to send request to update property value of an
         instance. If there is data parameter, the method gives updating. If
@@ -36,12 +36,12 @@ class ontology():
                   + '};'
                   + 'INSERT'
                   + 'DATA { '
-                  + 'kbase:{} kbase:{} {}}'.format(name, property, value))
+                  + 'kbase:{} kbase:{} {}'.format(name, property, value)
+                  + '}')
 
         # send POST request to server
         msg = {'update': self.prefix + update}
-        if name != "":
-            requests.post(url=self.ontology_server+'update', headers=self.header, data=msg)
+        requests.post(url=self.ontology_server+'update', headers=self.header, data=msg)
 
     def handle_instance(self, name, type, action):
         '''
@@ -73,3 +73,62 @@ class ontology():
             msg = {'update': self.prefix + update}
         if name != "":
             requests.post(url=self.ontology_server+'update', headers=self.header, data=msg)
+
+    def get_instances(self):
+        '''
+        This method gets list of data in Knowledge base.
+        '''
+        # Prepair message.
+        content = ('SELECT * '
+                    + 'WHERE { '
+                    + '?NAME a owl:NamedIndividual}')
+        msg = {'query': self.prefix + content}
+        r = requests.post(url=self.ontology_server+'query', headers=self.header, data=msg)
+        # Handle response as json type
+        res = json.loads(r.content)
+        # print(self.instance_list)
+        update_list = []
+        for i in res["results"]["bindings"]:
+            name = i["NAME"]["value"].split("#")[1]
+            update_list.append(name)
+        return update_list
+
+    def get_relationship(self):
+        '''
+        This method gets list of object properties in Knowledge base.
+        '''
+        # Prepair message.
+        content = ('SELECT * '
+                    + 'WHERE { '
+                    + '?NAME a owl:ObjectProperty}')
+        msg = {'query': self.prefix + content}
+        r = requests.post(url=self.ontology_server+'query', headers=self.header, data=msg)
+        # Handle response as json type
+        res = json.loads(r.content)
+        update_list = []
+        for i in res["results"]["bindings"]:
+            name = i["NAME"]["value"].split("#")[1]
+            update_list.append(name)
+        return update_list
+
+    def handle_relationship(self, relationship, *argv):
+        '''
+        This method updates the relationships between object instances.
+        '''
+        if argv:
+            name1 = argv[0]
+            name2 = argv[1]
+            # Prepair message
+            content = ('INSERT '
+                        + 'DATA { '
+                        + 'kbase:{} kbase:{} kbase:{}'.format(name1, relationship, name2)
+                        + '}')
+        else:
+            # Prepair message
+            content = ('INSERT '
+                        + 'DATA { '
+                        + 'kbase:{} a owl:ObjectProperty'.format(relationship)
+                        + '}')
+        msg = {'update': self.prefix + content}
+        print(msg)
+        requests.post(url=self.ontology_server+'update', headers=self.header, data=msg)
