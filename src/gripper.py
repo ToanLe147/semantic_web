356f46f254160b6efdaeb8565f56a52a7f307445
toanle147 <leductoan1210@gmail.com>
2019-10-19 03:54:53 +0300

    Complet Vacuum Gripper v1.02

--- src/gripper.py
+++ src/gripper.py
@@ -2,8 +2,8 @@
 import rospy
 from gazebo_ros_link_attacher.srv import Attach, AttachRequest
 from std_msgs.msg import String
-from gazebo_msgs.msg import LinkStates
-from gazebo_msgs.srv import SetModelState, GetModelState
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import SetModelState, GetModelState, GetLinkState
 from std_srvs.srv import Empty
 from uploader import Ontology
 
@@ -14,15 +14,14 @@ class Gripper:
     def __init__(self):
         # Gazebo Interface
         self.command = rospy.Subscriber('gripper_grasping', String, self.callback)
-        self.gazebo_conditions = rospy.Subscriber("/gazebo/link_states",
-                                                  LinkStates,
-                                                  self.gazebo_callback)
+        self.gazebo_conditions = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
         self.set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
         self.get_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
         self.attach_srv = rospy.ServiceProxy('/link_attacher_node/attach', Attach)
         self.detach_srv = rospy.ServiceProxy('/link_attacher_node/detach', Attach)
         self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
         self.unpause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
+        self.gazebo_conditions.wait_for_service()
         self.attach_srv.wait_for_service()
         self.detach_srv.wait_for_service()
         self.set_state.wait_for_service()
@@ -48,18 +47,22 @@ class Gripper:
         else:
             KnowledgeBase.update_property("Vacuum_gripper", "Data")
 
-    def gazebo_callback(self, msg):
-        gazebo_link_name = msg.name
-        gazebo_link_pose = msg.pose
+    def gazebo_callback(self, object):
+        gripper_link_name = self.gripper + "::" + self.gripper_link
+        object_link_name = object + "::" + self.common_link
+        gripper_pose = self.gazebo_conditions.call(gripper_link_name, "world")
+        object_pose = self.gazebo_conditions.call(object_link_name, "world")
+        gazebo_link_name = [gripper_link_name, object_link_name]
+        gazebo_link_pose = [gripper_pose, object_pose]
         self.gazebo_objects = dict(zip(gazebo_link_name, gazebo_link_pose))
 
     def grasping_condition(self, object):
         grasping = False
         gripper_link_name = self.gripper + "::" + self.gripper_link
         object_link_name = object + "::" + self.common_link
-        gripper_link_pose = self.gazebo_objects[gripper_link_name]
+        gripper_link_pose = self.gazebo_objects[gripper_link_name].link_state.pose
         try:
-            object_link_pose = self.gazebo_objects[object_link_name]
+            object_link_pose = self.gazebo_objects[object_link_name].link_state.pose
         except KeyError:
             print("Object name error")
             return grasping
@@ -76,15 +79,29 @@ class Gripper:
         return grasping
 
     def move_object(self, object):
+        # Prepare msg for set state ROS service
+        object_msg = ModelState()
+        object_msg.model_name = object
+
+        # Implement new pose for object
         self.pause.call()
         gripper = self.get_state.call(self.gripper, self.gripper_link)
         object = self.get_state.call(object, self.common_link)
-        object.pose.position.z = gripper.pose.position.z - 0.03
-        self.set_state.call(object)
+        object_msg.pose = object.pose
+        object_msg.twist = object.twist
+        object_msg.pose.position.z = gripper.pose.position.z - 0.03
+
+        print(gripper)
+        print("------------SEPERATE-------------")
+        print(object_msg)
+        # self.set_state.call(object)
+
+        # Release physic environment
         self.unpause.call()
         print("set state")
 
     def pick(self, object):
+        self.gazebo_callback(object)
         if self.grasping_condition(object):
             req = AttachRequest()
             self.status = True
